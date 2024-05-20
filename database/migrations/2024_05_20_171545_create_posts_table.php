e68d8025ad8fcd7a529af6fa2fcf9cdb1670bd3e
danielecolaci <danielecolaci@outlook.com>
2024-05-20 19:41:41 +0200

    Set crud, use faker

--- database/migrations/2024_05_20_171545_create_posts_table.php
+++ database/migrations/2024_05_20_171545_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150);
+            $table->string('slug', 150);
+            $table->text('content')->nullable();
+            $table->string('cover_image')->nullable();
             $table->timestamps();
         });
     }
