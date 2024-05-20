6589fea449169215ccf4f2498befefc54d960a1e
danielecolaci <danielecolaci@outlook.com>
2024-05-20 20:18:35 +0200

    Add index method to show posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +27,7 @@ Route::middleware(['auth', 'verified'])
     ->group(function () {
 
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
+        Route::resource('posts', PostController::class);
     });
 
 
